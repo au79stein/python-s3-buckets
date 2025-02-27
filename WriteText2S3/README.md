715d639cf17ddafe018a047c6b9d9d4ce62233e0
cloudnost <rich@cloudnost.com>
2025-02-27 14:58:01 -0500

    initial commit of writeText2s3

---
+++ WriteText2S3/README.md
@@ -0,0 +1,10 @@
+# write text directly to s3
+
+I was today years old when I realized that you can do this!
+You can write a string of text directly to s3 without having to create your own local file and then write that to s3.
+
+## potential use case
+
+there are obviously many, but for me I was thinking of taking the note that I collect from the command line and writing it directly to the s3 bucket.
+
+
---
+++ WriteText2S3/writeText2s3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import boto3
+
+BUCKET = "cloudnost"
+FILE_NAME = "/testing/wrote_string.txt"
+
+#Creating Session With Boto3.
+session = boto3.Session(
+)
+
+#Creating S3 Resource From the Session.
+s3 = session.resource('s3')
+
+txt_data = b'This is the content of the file uploaded from python boto3 asdfasdf'
+
+object = s3.Object(BUCKET, FILE_NAME)
+
+result = object.put(Body=txt_data)
